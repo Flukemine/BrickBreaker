19adfa7cd11fb3cb4b10f9f1d960837cc623c96a
dawson camilleri <dawson camilleri>
2018-01-18 18:25:38 +0100

    d

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour {
+
+    LevelManager levelManager = new LevelManager();
+
+    public int maxHits;
+
+    int timesHit;
+
+	// Use this for initialization
+	void Start () {
+
+        timesHit = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (this.tag == "Breakable")
+        {
+            timesHit++;
+            print(this.name + " : " + timesHit);
+
+            if (timesHit >= maxHits)
+            {
+                TestWin();
+                Destroy(gameObject);
+            }
+        }
+    } 
+
+    public void TestWin()
+    {
+        levelManager.LoadNextScene();
+    }
+
+}
